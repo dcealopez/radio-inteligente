a6bf2076bc3fad98571d47d64c4d21afbbbc403e
Diego Cea López <dcealopez@gmail.com>
2016-10-12 14:23:12 +0200

    Commit inicial

---
+++ res/recursos.h
@@ -0,0 +1,43 @@
+//
+// Escrito por: Diego Cea. (Doomer) - 2015
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPCION: Definición de las IDs de los recursos del programa.
+//
+
+#pragma once
+
+#define ID_ICONO											101
+#define ID_ICONO_TRAY										1001
+#define ID_ICONO_NOTIF										102
+#define IDD_DIAG_CONFIG										103
+#define ID_A_APOYO											40012
+#define ID_A_CENTRALITA										40005
+#define ID_A_CODIGOCINCO									40004
+#define ID_A_CUSTODIA										40019
+#define ID_A_HERIDO											40013
+#define ID_A_PANICO											40009
+#define ID_A_PAPAFOX										40017
+#define ID_A_PEDIDOS										40008
+#define ID_A_PEDIDOS_SWAT									40011
+#define ID_A_PROPIO											40007
+#define ID_A_REUNION										40002
+#define ID_A_ROBO_BANCO										40018
+#define ID_A_ROBO_CASA										40015
+#define ID_A_ROBO_COCHE										40014
+#define ID_A_ROBO_NEG										40016
+#define ID_A_SEISADAM										40003
+#define ID_A_SIN_ASIG										40010
+#define ID_A_REP_UNIDADES									40006
+#define	ID_AVISOS_ACTUAL									40022	
+#define ID_CONF_INDICATIVO									40020
+#define ID_CONF_MACRO										40021
